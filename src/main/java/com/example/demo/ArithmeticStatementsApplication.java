451fb895a78619e83335d934fc9eb9897f1c6b1b
Daniela Sainz <dasainz18@gmail.com>
2018-03-28 14:12:04 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/ArithmeticStatementsApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArithmeticStatementsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArithmeticStatementsApplication.class, args);
+	}
+}
